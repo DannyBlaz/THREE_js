9fd0a01bc1ad292d1e646677e7057ecdb07a976d
Daniel <dannyblaze469@gmail.com>
2022-05-01 12:06:41 -0700

    loaded materials

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -3,6 +3,35 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
+ * Textures
+ */
+// const loadingManager = new THREE.LoadingManager()
+
+// loadingManager.onStart = ()=>{
+//     console.log('onStart');
+// }
+// loadingManager.onLoad = ()=>{
+//     console.log('onLoad');
+// }
+// loadingManager.onProgress = ()=>{
+//     console.log('onProgress');
+// }
+// loadingManager.onError = ()=>{
+//     console.log('onError');
+// }
+
+const textureLoader = new THREE.TextureLoader()//loadingManager)
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorClphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+const matCapTexture = textureLoader.load('/textures/matcaps/6.png')
+
+/**
  * Base
  */
 // Canvas
@@ -10,6 +39,35 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const material = new THREE.MeshBasicMaterial()
+// material.wireframe = true
+// material.map = doorAmbientOcclusionTexture
+// material.side = THREE.DoubleSide 
+material.opacity = 0.5
+material.transparent = true
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 100),
+    material
+) 
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
 
 /**
  * Sizes
@@ -65,6 +123,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update Objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+    
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
     // Update controls
     controls.update()
