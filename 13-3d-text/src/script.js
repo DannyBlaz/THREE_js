3995d5d5a48b7bca674009671b1345d3b0e76b57
Daniel <dannyblaze469@gmail.com>
2022-05-01 22:02:52 -0700

    changed text

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -32,7 +32,7 @@ fontLoader.load(
     '/fonts/helvetiker_regular.typeface.json',
     (font) => {
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Danny Blaze',
             {
                 font: font,
                 size: 0.5,
